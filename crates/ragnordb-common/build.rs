931d32dc22f843a975156399af14386a4d6fb130
Aditya Mandal <mandaladitya1614@gmail.com>
2026-05-19 18:05:59 +0530

    added a build script for protocol buffers, so that they can be auto converted to rust types using prost crate

---
+++ crates/ragnordb-common/build.rs
@@ -0,0 +1,21 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let proto_dir = "../..proto";
+    let protos = &[
+        "proto/ids.proto",
+        "proto/row.proto",
+        "proto/catalog.proto",
+        "proto/mvcc.proto",
+        "proto/command.proto",
+        "proto/rpc.proto",
+    ];
+
+    std::fs::create_dir_all("src/proto")?;
+
+    let mut config = prost_build::Config::new();
+    config.out_dir("src/proto");
+
+    config.compile_protos(protos, &[proto_dir])?;
+
+    println!("cargo:rerun-if-changed=../../proto");
+    Ok(())
+}
